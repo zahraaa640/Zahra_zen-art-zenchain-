f03e5cb17009b6389875f356a0b9b29a716183a0
zahraaa640 <zahrabakhodabn@gmail.com>
2025-10-04 20:16:47 +0330

    Update connectWallet.js

--- connectWallet.js
+++ connectWallet.js
@@ -1,1 +1,14 @@
-
+async function connectWallet() {
+  if (window.ethereum) {
+    try {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const userAddress = accounts[0];
+      console.log("Wallet connected:", userAddress);
+      return userAddress;
+    } catch (error) {
+      console.error("Connection error:", error);
+    }
+  } else {
+    alert("MetaMask not found. Please install it.");
+  }
+}
